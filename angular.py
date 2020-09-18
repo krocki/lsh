43b16031415f9ff9602034412e1ade0bf1bc61cc
Kamil Rocki <kamil.rocki@gmail.com>
2020-09-18 16:45:31 -0700

    ang

--- angular.py
+++ angular.py
@@ -12,7 +12,7 @@ import time
 # n - num of input buckets
 # h - num of hashes
 
-np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
+np.set_printoptions(formatter={'float': '{: 0.2f}'.format})
 
 def cosine_sim(v,w):
   return np.dot(v,w)/(np.linalg.norm(v) * np.linalg.norm(w))
@@ -37,34 +37,32 @@ def lsh_hyperplane(x, h):
 
   return (projections >= 0).astype(np.int32)
 
-def lsh_angular(x, n, h):
+def lsh_angular(x, b, h):
 
-  rot_size = n
-  n_buckets = n
-  rotations_shape = (x.shape[-1], h, rot_size // 2)
+  print('lsh_angular():\nx={},\nb={}, h={}'.format(x, b, h))
+  rotations_shape = (x.shape[-1], h, b // 2)
 
-  print('lsh_angular():\nx={},\nn={}, h={}'.format(x, n, h))
   print('rotations_shape')
   print(rotations_shape)
   debug_print(x, 'x')
 
-  r_rotations = np.random.randn(*rotations_shape).astype(np.float32)
-  r_rotations = np.reshape(r_rotations, [-1, h * (rot_size // 2)])
-  debug_print(r_rotations, 'r_rotations')
+  rr = np.random.randn(*rotations_shape).astype(np.float32)
+  rr = np.reshape(rr, [-1, h * b // 2])
+  debug_print(rr, 'rr')
 
-  rotated_vecs = np.dot(x, r_rotations)
-  debug_print(rotated_vecs, 'rotated_vecs')
+  rv = np.dot(x, rr)
+  debug_print(rv, 'rv')
 
-  rotated_vecs = np.reshape(rotated_vecs, [-1, h, rot_size // 2])
-  debug_print(rotated_vecs, 'reshape rotated_vecs')
+  rv = np.reshape(rv, [-1, h, b // 2])
+  debug_print(rv, 'reshape rv')
 
-  rotated_vecs = np.transpose(rotated_vecs, (1, 0, 2))
-  debug_print(rotated_vecs, 'transpose rotated_vecs')
+  rv = np.transpose(rv, (1, 0, 2))
+  debug_print(rv, 'transpose rv')
 
-  rotated_vecs = np.concatenate([rotated_vecs, -rotated_vecs], axis=-1)
-  debug_print(rotated_vecs, 'concatenate')
+  rv = np.concatenate([rv, -rv], axis=-1)
+  debug_print(rv, 'concatenate')
 
-  buckets = np.argmax(rotated_vecs, axis=-1).astype(np.int32)
+  buckets = np.argmax(rv, axis=-1).astype(np.int32)
   debug_print(buckets, 'buckets')
 
   return buckets
@@ -72,29 +70,23 @@ def lsh_angular(x, n, h):
 if __name__ == "__main__":
 
   parser = argparse.ArgumentParser(description='')
-  parser.add_argument('-N', type=int, default=5,  help='input size')
-  parser.add_argument('-d', type=int, default=8,  help='input dim')
-  parser.add_argument('-I', type=int, default=4,  help='number of buckets in')
-  parser.add_argument('-O', type=int, default=3,  help='number of buckets')
+  parser.add_argument('-N', type=int, default=3,  help='input size')
+  parser.add_argument('-D', type=int, default=2,  help='input dim')
+  parser.add_argument('-H', type=int, default=4,  help='number of hashes')
+  parser.add_argument('-B', type=int, default=4,  help='number of buckets')
 
   args = parser.parse_args()
-  N, d, I, O = args.N, args.d, args.I, args.O
+  N, D, H, B = args.N, args.D, args.H, args.B
 
-  #vecs = np.random.randn(N, d)
-  vecs = np.array([
-    [ 1, 0, 0, 1, 1, 1, 0, 0],
-    [ 1, 0, 0, 1, 1, 0, 0, 0],
-    [ 0, 0, 1, 0, 0, 0, 0, 0],
-    [ 0, 1, 1, 1, 0, 0, 0, 0],
-    [ 0, 1, 1, 1, 1, 0, 0, 0]
-  ])
+  vecs = np.random.uniform(size=(N, D)).astype(np.float32)
 
-  hashes = lsh_angular(vecs, I, O).T
+  hashes = lsh_angular(vecs, B, H).T
   debug_print(hashes, 'hashes')
 
   sim = np.array([cosine_sim(v, w) for v in vecs for w in vecs]).reshape(N,N)
   debug_print(sim, 'cosine sim')
 
+  debug_print(vecs, 'x')
   #print('hyperplane')
   #hashes0 = lsh_hyperplane(vecs, O)
   #debug_print(hashes0, 'hashes hyperplane')
